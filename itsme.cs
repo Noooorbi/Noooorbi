9285e6d804af46555c0f5fc0ea884fb3a1a3df49
Nbi <tyburski.norbi@gmail.com>
2024-11-07 17:27:48 +0100

    itsme.cs

---
+++ itsme.cs
@@ -0,0 +1,20 @@
+class Nbi:
+    def __init__(self):
+        self.name = 'Norbert'
+        self.nick = 'Nbi'
+    
+    def whoami(self):
+        self.home = 'PL'
+        self.year = 2004
+        self.province = 'Lubuskie'
+        self.pet = 'German Shepherd'
+    
+    def skills(self):
+        self.mainlanguage = 'lua'
+        self.languages = ['python', 'html', 'lua']
+        
+me = Nbi()
+<!---
+Noooorbi/Noooorbi is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
